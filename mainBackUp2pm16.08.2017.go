967fadac1972643906dd960b894126bc08f1c4e8
sarah_anderson <anderson.sarahgrace@gmail.com>
2017-08-21 10:36:37 +1200

    Wednesday progress

--- mainBackUp2pm16.08.2017.go
+++ mainBackUp2pm16.08.2017.go
@@ -61,26 +61,27 @@ func main() {
 	  -- Validate the amount of data (count( ))
 	*/
 
-	studentReport := generateStudentReport(studentData)
-
+	//studentReport := generateStudentReport(studentData)
+	//fmt.Println(studentReport)
 	// Print data on the screen
-	formatted, _ := json.MarshalIndent(studentReport, "", "\t")
-	fmt.Println(string(formatted)) // Uncomment this line for printout of file contents
+	//formatted, _ := json.MarshalIndent(studentReport, "", "\t")
+	//fmt.Println(string(formatted)) // Uncomment this line for printout of file contents
+	generateStudentReport(studentData)
 
 }
 
 /*
 Select all students and show the marks for each student
 */
-func generateStudentReport(studentData StudentData) []StudentReportRecord {
-	var studentReport []StudentReportRecord
+//func generateStudentReport(studentData StudentData)([]StudentReportRecord){
+func generateStudentReport(studentData StudentData) {
+	//var studentReport []StudentReportRecord
 
 	startTime := time.Now()
 	fmt.Println(startTime.Format("Mon Jan 2 2006 15:04:05.000"))
 
 	// Make a useful map
 	markMap := make(map[float64][]CourseMark)
-
 	for studentDataIndex := 0; studentDataIndex < len(studentData.MyMarks); studentDataIndex++ {
 		currentStudentID := studentData.MyMarks[studentDataIndex].StudentID
 		var currentArray []CourseMark
@@ -93,29 +94,27 @@ func generateStudentReport(studentData StudentData) []StudentReportRecord {
 		markMap[currentStudentID] = currentArray
 	}
 
-	// Format the data in the map
-	for studentDataIndex := 0; studentDataIndex < len(studentData.MyStudents); studentDataIndex++ {
-		studentReportRecord := StudentReportRecord{
-			FirstName: studentData.MyStudents[studentDataIndex].FirstName,
-			LastName:  studentData.MyStudents[studentDataIndex].LastName,
-			Marks:     make([]CourseMark, 0)}
-
-		for markIndex := 0; markIndex < len(markMap[studentData.MyStudents[studentDataIndex].StudentID]); markIndex++ {
-			newMark := CourseMark{
-				Class: markMap[studentData.MyStudents[studentDataIndex].StudentID][markIndex].Class,
-				Mark:  markMap[studentData.MyStudents[studentDataIndex].StudentID][markIndex].Mark,
-			}
-
-			studentReportRecord.Marks = append(studentReportRecord.Marks, newMark)
-		}
-		studentReport = append(studentReport, studentReportRecord)
-	}
-
 	endTime := time.Now()
 	fmt.Println(endTime.Format("Mon Jan 2 2006 15:04:05.000"))
-	fmt.Print("Used time: ", endTime.Sub(startTime))
+	diff := endTime.Sub(startTime)
+	fmt.Print("Used time: ", diff)
+	//fmt.Println(diff.Format("Mon Jan 2 2006 15:04:05.000"))
 
-	return studentReport
+	// Format the data in the map
+	for studentDataIndex := 0; studentDataIndex < len(studentData.MyStudents); studentDataIndex++ {
+		fmt.Println("\n", studentData.MyStudents[studentDataIndex].FirstName, studentData.MyStudents[studentDataIndex].LastName)
+
+		for markIndex := 0; markIndex < len(markMap[studentData.MyStudents[studentDataIndex].StudentID]); markIndex++ {
+			fmt.Printf("    | %-20s|%6.2f |\n", markMap[studentData.MyStudents[studentDataIndex].StudentID][markIndex].Class, markMap[studentData.MyStudents[studentDataIndex].StudentID][markIndex].Mark)
+		}
+		/*studentReportRecord := StudentReportRecord{
+			FirstName: "Sarah",
+			LastName: "Anderson"}
+		studentReport = append(studentReport, studentReportRecord)
+		*/
+	}
+
+	//return studentReport
 }
 
 /*
@@ -133,7 +132,7 @@ func importAndUnmarshal(fileName string) StudentData {
 	myErr := json.Unmarshal(jsonBlob, &fileContent)
 	check(myErr)
 
-	//fmt.Println(fileContent)
+	fmt.Println(fileContent)
 	return fileContent
 }
 
